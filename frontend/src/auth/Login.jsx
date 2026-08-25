54c1a0dc46c95115a468a10486d337bc4cbab629
Jeevan s <sathishjeevan2004@gmail.com>
2026-08-25 14:45:47 +0530

    handling pacakge

--- frontend/src/auth/Login.jsx
+++ frontend/src/auth/Login.jsx
@@ -1,11 +1,24 @@
 import { GoogleLogin } from "@react-oauth/google";
+import axios from "axios";
 
 const Login = () => {
+  async function handleLogin(credential) {
+    if (credential) {
+      console.log(credential);
+    }
+    try {
+      const response = await axios.post("http://localhost:3000/login", {
+        token: credential.credentail,
+      });
+      const result = response.data.message;
+      console.log(result);
+    } catch (error) {
+      console.log(error);
+    }
+  }
   return (
     <GoogleLogin
-      onSuccess={(credential) => {
-        console.log(credential.credential);
-      }}
+      onSuccess={handleLogin}
       onError={() => {
         console.log("google login failed");
       }}
--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -2,8 +2,19 @@ import Login from "../auth/Login";
 
 const LoginPage = () => {
   return (
-    <div className="w-full h-screen bg-black">
-      <Login />
+    <div className="w-full h-screen flex flex-col justify-center items-center bg-black">
+      <div className="w-[400px] h-[500px] text-black flex flex-col justify-center items-center gap-2 rounded-2xl bg-zinc-900 ">
+        <div>
+          <p className=" animate-pulse text-white text-[24px]">
+            wellcome`! LogIn with our site{" "}
+          </p>
+          <h1 className="text-[40px] font-extralight text-white">
+            Prompt
+            <span className="font-extrabold text-amber-900">Go</span>
+          </h1>
+        </div>
+        <Login />
+      </div>
     </div>
   );
 };
