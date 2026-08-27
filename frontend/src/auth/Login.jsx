bdabe246e1c5e5ffce4f53e38e838d81bd9ecae2
Jeevan s <sathishjeevan2004@gmail.com>
2026-08-27 11:11:35 +0530

    redux

--- frontend/src/auth/Login.jsx
+++ frontend/src/auth/Login.jsx
@@ -1,15 +1,36 @@
 import { GoogleLogin } from "@react-oauth/google";
+import { useSelector, useDispatch } from "react-redux";
 
 import { userLogin } from "../reduxState/authThunkFun/userLogin";
 
 const Login = () => {
+  const { loading, error, user } = useSelector((state) => state.auth);
+
+  const dispatch = useDispatch();
+
+  const handleSuccess = (credentialResponse) => {
+    dispatch(
+      userLogin({
+        credential: credentialResponse.credential,
+      }),
+    );
+  };
+
   return (
-    <GoogleLogin
-      onSuccess={userLogin}
-      onError={() => {
-        console.log("google login failed");
-      }}
-    />
+    <div>
+      <GoogleLogin
+        onSuccess={handleSuccess}
+        onError={() => {
+          console.log("Google login failed");
+        }}
+      />
+
+      {loading && <p>Logging in...</p>}
+
+      {error && <p>{error}</p>}
+
+      {user && <p>Login successful</p>}
+    </div>
   );
 };
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,15 +2,22 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+
+import { store } from "./reduxState/authstate/store";
+
 const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <GoogleOAuthProvider clientId={clientId}>
       <BrowserRouter>
-        <App />
+        <Provider store={store}>
+          <App />
+        </Provider>
       </BrowserRouter>
     </GoogleOAuthProvider>
   </StrictMode>,
